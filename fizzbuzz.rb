7b28e264e30de3f6f65d3d80d65d0827ac0e37f3
Rajvi Chavada <rajvic@my.yorku.ca>
2018-05-19 22:46:11 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,11 @@
 def fizzbuzz(int)
+  if int % 3 == 0 && int % 5 == 0
+    "FizzBuzz"
 
-if int % 3 == 0
-  "Fizz"
 else if int % 5 == 0
   "Buzz"
-else if int % 3 == 0 and int % 5 == 0
-  "FizzBuzz"
+else if if int % 3 == 0
+  "Fizz"
 else
 end
 end
